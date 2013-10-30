b83f0593f51df7be42e4a2aef256fdc1c99722df
defter1760 <defter@gmail.com>
2013-10-30 09:07:55 -0700

    added an ad

---
+++ ads/LGUnderHS.php
@@ -0,0 +1,11 @@
+<?PHP
+echo '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- LG under highscore -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-9427481745268298"
+     data-ad-slot="5789986463"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>';
+?>
