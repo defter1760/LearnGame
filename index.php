338c0f78ef07815f6c4d3f900199cf0bc5f175fe
defter1760 <defter@gmail.com>
2013-10-30 08:47:58 -0700

    table update

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 echo 'Hello Werld!';
 echo date('d');
 
-echo '<table border=2>';
+echo '<table border=2 width="100%">';
     echo '<tr>';
-        echo '<td width="160px">';
+        echo '<td width="200px">';
         echo '</td>';
         echo '<td>';
         echo '</td>';
-        echo '<td width="160px">';
+        echo '<td width="200px">';
             echo '[Login] [User] [*Pass]';
         echo '</td>';
     echo '</tr>';
