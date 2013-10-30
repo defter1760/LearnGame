ee56979c3481d9641e857a91cabfaeb0554c3598
defter1760 <defter@gmail.com>
2013-10-30 11:23:20 -0700

    test count

--- game/assignments.php
+++ game/assignments.php
@@ -1,4 +1,31 @@
 <?PHP
+require('mySQLconnect.php');
+$typeint = rand(1, 1);
+
+if($typeint == '1')
+{
+    $type = 'command';
+
+    $query = "SELECT count(*) FROM";
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());    
+    while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
+    {
+        $commandcount= $line['count'];
+    }
+    echo 'COUNT:';
+echo $commandcount;    
+    $correctint = rand(1, 6);
+}
+if($typeint == '2')
+{
+    $type = 'context';
+}
+if($typeint == '3')
+{
+    $type = 'reversecommand';
+}
+if($typeint == '4')
+
 $type = 'command';
 $question = 'cp';
     $a = 'copy a file';
--- game/score/getplayerscore.php
+++ game/score/getplayerscore.php
@@ -1,3 +1,3 @@
 <?PHP
-    echo 'getplayerscore';
+    echo ' ## getplayerscore';
 ?>
