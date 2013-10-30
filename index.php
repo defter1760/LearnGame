f8abc6d8cf6e751d2df33141bf0f85db0c02fc95
defter1760 <defter@gmail.com>
2013-10-30 08:46:23 -0700

    start table

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?PHP
 echo 'Hello Werld!';
 echo date('d');
+
+echo '<table border=2>';
+    echo '<tr>';
+        echo '<td width="160px">';
+        echo '</td>';
+        echo '<td>';
+        echo '</td>';
+        echo '<td width="160px">';
+            echo '[Login] [User] [*Pass]';
+        echo '</td>';
+    echo '</tr>';
 ?>
