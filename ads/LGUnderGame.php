64cffdce0364ad40a209ddcbdbe1a28687980311
defter1760 <defter@gmail.com>
2013-10-30 09:16:19 -0700

    add an ad

---
+++ ads/LGUnderGame.php
@@ -0,0 +1,11 @@
+<?PHP
+    echo '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- LG Under Game -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-9427481745268298"
+     data-ad-slot="2138516068"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>';
+?>
