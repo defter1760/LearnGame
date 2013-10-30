527a73b57247d78b3655d643e04bd68b8dd2618a
defter1760 <defter@gmail.com>
2013-10-30 09:13:39 -0700

    add right bar ad

---
+++ ads/LGRightBar.php
@@ -0,0 +1,11 @@
+<?PHP
+echo '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- LG right side bar -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:120px;height:600px"
+     data-ad-client="ca-pub-9427481745268298"
+     data-ad-slot="9801383668"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>';
+?>
