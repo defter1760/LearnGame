72a0465e677fa92ea3033eccc22dea15db24c8b7
defter1760 <defter@gmail.com>
2013-10-31 09:17:09 -0700

    access update

---
+++ game/logout.php
@@ -0,0 +1,26 @@
+<?php
+#require('head.php');
+    session_start();
+    $_SESSION['loggedIn'] = false;
+?>
+
+<?PHP
+require('index.php');
+?>
+<!--<table style="border:1px solid black;">
+    <tr>
+	<td>
+	
+	    <h1>Resume Sniper
+	    </h1>
+	    <table>
+		<tr>
+		    <td>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+<br><br>-->
+<!--<a href=index.php>Home</a>-->
