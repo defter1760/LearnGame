a5125aee47474329fc5f825163dd0f16c54a2c48
defter1760 <defter@gmail.com>
2013-10-31 09:43:49 -0700

    access update

--- game/checkpost.php
+++ game/checkpost.php
@@ -15,14 +15,39 @@ if($submitted_type == 'command')
     {
         $truedescription= $line['description'];
     }
+
+    $query = "SELECT score FROM userdata where iduserdata = '".$_SESSION['UserID']."'";
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());    
+    while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
+    {
+        $oldscore= $line['score'];
+    }
+    
+
     if($truedescription == $submitted_answer)
     {
+        
+            $newscore = $oldscore++;
+            $query = "UPDATE userdata set score='".$newscore."' where iduserdata = '".$_SESSION['UserID']."'";
+            $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+        
         echo 'That\'s right! '.$submitted_question.'='.$truedescription;
     }
     else
     {
+            $newscore = $oldscore--;
+            $query = "UPDATE userdata set score='".$newscore."' where iduserdata = '".$_SESSION['UserID']."'";
+            $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+        
         echo 'Wrong, '.$submitted_question.'='.$truedescription;
     }
+$score = $newscore;
 }
+
+if(isset $score)
+{
+    echo '<br>Score: '.$score;
+}
+
 
 ?>
