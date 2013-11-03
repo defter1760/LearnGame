481330d09d1d9b4ed243b58738520ee1ae80bf3c
defter1760 <defter@gmail.com>
2013-11-03 11:45:14 -0800

    highscore update

--- game/highscore.php
+++ game/highscore.php
@@ -5,29 +5,30 @@ require('mySQLconnect.php');
     echo '<table border=1>';
         echo '<tr>';
             
-    $query = "SELECT username, score FROM userdata ORDER BY score DESC LIMIT 10";
-    $result = mysql_query($query) or die('Query failed: ' . mysql_error());    
-    
-    while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
-    {
-        $counter +=1;
-        if($counter == 1)
+        $query = "SELECT username, score FROM userdata ORDER BY score DESC LIMIT 10";
+        $result = mysql_query($query) or die('Query failed: ' . mysql_error());    
+        
+        while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
         {
-            echo '<td>';
-                echo $line['username'].' [ '.$line['score'].' ]<br>';
+            $counter +=1;
+            if($counter == 1)
+            {
+                echo '<td>';
+                    echo $line['username'].' [ '.$line['score'].' ]<br>';
+            }
+            if($counter == 5)
+            {
+                echo '</td>';
+                echo '<td>';
+            }
+            #$topusers[]= $line['username'];
+            #$topscores[]= $line['score'];
+            if($counter == 10)
+            {
+                echo '</td>';
+            }
         }
-        if($counter == 5)
-        {
-            echo '</td>';
-            echo '<td>';
-        }
-        #$topusers[]= $line['username'];
-        #$topscores[]= $line['score'];
-        if($counter == 10)
-        {
-            echo '</td>';
-        }
-    }
-
+    echo '</tr>';
+echo '</table>';
 
 ?>
