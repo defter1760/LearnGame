28172367673dc46e968594d0744b53a882d2c9f8
defter1760 <defter@gmail.com>
2013-11-04 08:38:58 -0800

    add port questions

--- game/assignments.php
+++ game/assignments.php
@@ -1,6 +1,6 @@
 <?PHP
 require('mySQLconnect.php');
-$typeint = rand(1, 4);
+$typeint = rand(1, 6);
 
 if($typeint == '1')
 {
@@ -241,8 +241,124 @@ if($typeint == '4')
 }
 if($typeint == '5')
 {
-    $type = 'networkingadv';
+    $type = 'ports';
+    
+        
+    $query = "SELECT count(*) as COUNT FROM ports";
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());    
+    while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
+    {
+        $commandcount= $line['COUNT'];
+    }
+    
+    $commandselection = rand(1, $commandcount);
+    
+    $query = "SELECT * FROM ports where portid='".$commandselection."'";
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());    
+    while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
+    {
+        $answer= $line['answer'];
+        $question= $line['question'];
+    }
+    
+    $query = "SELECT answer FROM ports where portid != '".$commandselection."' ORDER BY RAND() LIMIT 7";
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());    
+    while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
+    {
+        $answers[]= $line['answer'];
+    }
+    
+    $a= $answers['1'];
+    $b= $answers['2'];
+    $c= $answers['3'];
+    $d= $answers['4'];
+    $e= $answers['5'];
+    $f= $answers['6'];
+    
+    $correctint = rand(1, 6);
+    
+    switch ($correctint) {
+        case 1:
+            $a = $answer;
+            break;
+        case 2:
+            $b = $answer;
+            break;
+        case 3:
+            $c = $answer;
+            break;
+        case 4:
+            $d = $answer;
+            break;
+        case 5:
+            $e = $answer;
+            break;
+        case 6:
+            $f = $answer;
+            break;   
+    } 
 }
+
+if($typeint == '6')
+{
+    $type = 'reverseports';
+    
+        
+    $query = "SELECT count(*) as COUNT FROM ports";
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());    
+    while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
+    {
+        $commandcount= $line['COUNT'];
+    }
+    
+    $commandselection = rand(1, $commandcount);
+    
+    $query = "SELECT * FROM ports where portid='".$commandselection."'";
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());    
+    while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
+    {
+        $answer= $line['question'];
+        $question= $line['answer'];
+    }
+    
+    $query = "SELECT question FROM ports where portid != '".$commandselection."' ORDER BY RAND() LIMIT 7";
+    $result = mysql_query($query) or die('Query failed: ' . mysql_error());    
+    while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
+    {
+        $answers[]= $line['question'];
+    }
+    
+    $a= $answers['1'];
+    $b= $answers['2'];
+    $c= $answers['3'];
+    $d= $answers['4'];
+    $e= $answers['5'];
+    $f= $answers['6'];
+    
+    $correctint = rand(1, 6);
+    
+    switch ($correctint) {
+        case 1:
+            $a = $answer;
+            break;
+        case 2:
+            $b = $answer;
+            break;
+        case 3:
+            $c = $answer;
+            break;
+        case 4:
+            $d = $answer;
+            break;
+        case 5:
+            $e = $answer;
+            break;
+        case 6:
+            $f = $answer;
+            break;   
+    } 
+}
+
 //$type = 'command';
 //$question = 'cp';
 //    $a = 'copy a file';
